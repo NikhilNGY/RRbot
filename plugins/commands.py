18556e88c1aa75949f7cdd242dfcb7eefe017972
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2025-09-11 23:32:15 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -332,8 +332,8 @@ async def start(client, message):
                 )
             )
             filesarr.append(msg)
-        k = await client.send_message(chat_id = message.from_user.id, text=f"<b><u>🔴 IMPORTANT 🔴</u></b>\n\nThis Movie Files/Videos will be deleted in <b><u>20 mins</u><i></b>(Due to Copyright Issues)</i>.\n\n<b><i>Please forward this ALL Files/Videos to Someone and Start Download there</i></b>\n\n<b><u>🔴 IMPORTANT 🔴</u></b>")
-        await asyncio.sleep(1200)
+        k = await client.send_message(chat_id = message.from_user.id, text=f"<b><u>🔴 IMPORTANT 🔴</u></b>\n\nThis Movie Files/Videos will be deleted in <b><u>10 mins</u><i></b>(Due to Copyright Issues)</i>.\n\n<b><i>Please forward this ALL Files/Videos to Someone and Start Download there</i></b>\n\n<b><u>🔴 IMPORTANT 🔴</u></b>")
+        await asyncio.sleep(600)
         for x in filesarr:
             await x.delete()
         await k.edit_text("<b>Your All Files/Videos is successfully deleted!!!</b>")
